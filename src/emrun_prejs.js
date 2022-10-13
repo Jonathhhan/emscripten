be9a921ee32332eb587f04f5bdcac41bcdfa9c09
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-10-13 04:10:49 +0200

    Update emrun_prejs.js

--- src/emrun_prejs.js
+++ src/emrun_prejs.js
@@ -5,7 +5,6 @@
  */
 
 // Route URL GET parameters to argc+argv
-if (!ENVIRONMENT_IS_AUDIOWORKLET)
 if (typeof window === "object") {
   Module['arguments'] = window.location.search.substr(1).trim().split('&');
   // If no args were passed arguments = [''], in which case kill the single empty string.
